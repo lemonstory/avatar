fa739df065998db439ec63b63fe7b526e2552850
huqiangqiang <will8436992@sina.com>
2015-11-03 16:59:43 +0800

    addadd showavatar.php

---
+++ showavatar.php
@@ -0,0 +1,29 @@
+<?php
+$uid = $_GET['uid'];
+$size = $_GET['size'];
+$avatartime = $_GET['avatartime'];
+
+$domain = "http://aoss.xiaoningmeng.net/"; // oss中lemonavatar的cdn域名
+$osspath = $domain . $uid . "?v=" . $avatartime;
+if($size > 0) {
+    if(!in_array($size ,array(80, 100, 120))) {
+        $size = 120;
+    }
+    $osspath = $domain . $uid . "@!{$size}x{$size}" . "?v=" . $avatartime;
+}
+
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $osspath);
+curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+curl_setopt($ch, CURLOPT_REFERER, $osspath);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$content = curl_exec($ch);
+curl_close($ch);
+
+header("content-type: image/jpeg");
+if(strlen($content) < 1000) {
+    $defaultfile = $size . "-" . ($uid%5+1) . ".png";
+    echo file_get_contents(dirname(__FILE__) . "/static" . $defaultfile);
+}else{
+    echo $content;
+}
